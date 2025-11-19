353d2704bc40cbcf7cce923d773df9936a166891
João Almeida <mr.johnplay0108@gmail.com>
2025-11-18 20:32:30 -0500

    Quase finalizado o home

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,9 +1,37 @@
 import styles from './Home.module.css'
 
+import Button from '../../components/Button/Button'
+
+import { FaRegPlusSquare } from "react-icons/fa";
+
+
+
 export default function Home() {
+
+  let valueBalance = 20.1
+
   return (
     <>
-    
+      <section className={styles.section}>
+        <div className={styles.containerGraphic}>
+          <div className={styles.cardGraphic}>
+            <div className={styles.buttonGraphic}>
+              <Button size='large' version='greenBck'>Gráfico</Button>
+              <Button size='large' version='greenBck'>Histótico</Button>
+            </div>
+            <div className="graphic"></div>
+          </div>
+        </div>
+        <div className={styles.containerValue}>
+          <div className={styles.cardValue}>
+            <div className={styles.number}>
+              <span className={styles.span}>Saldo Atual</span>
+              <span>R$ {valueBalance}</span>
+            </div>
+            <FaRegPlusSquare className={styles.iconPlus} />
+          </div>
+        </div>
+      </section>
     </>
   )
 }
